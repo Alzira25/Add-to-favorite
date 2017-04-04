a958b7e9126f0cd29657df078a00022a0a560419
Alzira25 <alzirabarretto@gmail.com>
2017-04-04 12:15:11 +0530

    Create design-details.php

---
+++ design-details.php
@@ -0,0 +1,8 @@
+ <!--Get Product details from database -->
+ #code....
+ #code...
+ 
+ <div class="favourites">
+    <?php addFavorites(); ?>
+      <a href="design-details.php?add_favorite=<?php echo $pro_id; ?>" style="width: 50%;" class="btn btn-primary center-block" id="fav" type="button">Add to Favourite</a>
+ </div> 
